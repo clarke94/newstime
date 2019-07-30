b6a5ff8512a3aff642adc5a4996fc96ac44c617d
Liam Clarke <liam.clarke.94@outlook.com>
2019-07-30 21:07:40 +0100

    Add Tabs Refactor

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -14,32 +14,17 @@ export class TabsComponent implements OnInit, OnDestroy {
     @Output() pageChange: EventEmitter<number> = new EventEmitter<number>();
 
     topHeadlinesService;
-    topHeadlines: Article[];
+    newsByCategoryService;
+
     latestPage = 1;
-
-    businessService;
-    business: Article[];
     businessPage = 1;
-
-    entertainmentService;
-    entertainment: Article[];
     entertainmentPage = 1;
-
-    healthService;
-    health: Article[];
     healthPage = 1;
-
-    scienceService;
-    science: Article[];
     sciencePage = 1;
-
-    sportsService;
-    sports: Article[];
     sportsPage = 1;
-
-    technologyService;
-    technology: Article[];
     technologyPage = 1;
+
+    data = {};
 
     constructor(
         private dataService: DataService,
@@ -52,12 +37,7 @@ export class TabsComponent implements OnInit, OnDestroy {
 
     ngOnDestroy() {
         this.topHeadlinesService.unsubscribe();
-        this.businessService.unsubscribe();
-        this.entertainmentService.unsubscribe();
-        this.healthService.unsubscribe();
-        this.scienceService.unsubscribe();
-        this.sportsService.unsubscribe();
-        this.technologyService.unsubscribe();
+        this.newsByCategoryService.unsubscribe();
     }
 
     getNewsByCategory(event) {
@@ -65,69 +45,10 @@ export class TabsComponent implements OnInit, OnDestroy {
         const params = {
             category: `category=${category}`
         };
-        switch (category) {
-            case 'business':
-                if (!this.business) {
-                    console.log('Paginate', this.businessPage);
-                    this.getBusinessNews(params);
-                }
-                break;
-            case 'entertainment':
-                if (!this.entertainment) {
-                    this.getEntertainmentNews(params);
-                }
-                break;
-            case 'health':
-                if (!this.health) {
-                    this.getHealthNews(params);
-                }
-                break;
-            case 'science':
-                if (!this.science) {
-                    this.getScienceNews(params);
-                }
-                break;
-            case 'sports':
-                if (!this.sports) {
-                    this.getSportsNews(params);
-                }
-                break;
-            case 'technology':
-                if (!this.technology) {
-                    this.getTechnologyNews(params);
-                }
-                break;
-            default:
-                this.getTopHeadlines();
-        }
-
+        this.newsByCategoryService = this.newsService.getNews(params).subscribe(data => this.data[category] = data.articles);
     }
 
     getTopHeadlines() {
-        this.topHeadlinesService = this.dataService.getTopHeadlines().subscribe(data => this.topHeadlines = data);
-    }
-
-    getBusinessNews(params) {
-        this.businessService = this.newsService.getNews(params).subscribe(data => this.business = data.articles);
-    }
-
-    getEntertainmentNews(params) {
-        this.entertainmentService = this.newsService.getNews(params).subscribe(data => this.entertainment = data.articles);
-    }
-
-    getHealthNews(params) {
-        this.healthService = this.newsService.getNews(params).subscribe(data => this.health = data.articles);
-    }
-
-    getScienceNews(params) {
-        this.scienceService = this.newsService.getNews(params).subscribe(data => this.science = data.articles);
-    }
-
-    getSportsNews(params) {
-        this.sportsService = this.newsService.getNews(params).subscribe(data => this.sports = data.articles);
-    }
-
-    getTechnologyNews(params) {
-        this.technologyService = this.newsService.getNews(params).subscribe(data => this.technology = data.articles);
+        this.topHeadlinesService = this.dataService.getTopHeadlines().subscribe(data => this.data['featured'] = data);
     }
 }
