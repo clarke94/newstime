e8f447b8b459c2658fb1cfc69d1f9b991822c98a
Liam Clarke <liam.clarke.94@outlook.com>
2019-08-04 13:40:58 +0100

    Add Hero Component unit tests

--- src/app/components/hero-featured/hero-featured.component.spec.ts
+++ src/app/components/hero-featured/hero-featured.component.spec.ts
@@ -1,25 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroFeaturedComponent } from './hero-featured.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HeroFeaturedComponent', () => {
-  let component: HeroFeaturedComponent;
-  let fixture: ComponentFixture<HeroFeaturedComponent>;
+    let component: HeroFeaturedComponent;
+    let fixture: ComponentFixture<HeroFeaturedComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HeroFeaturedComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [ReactiveFormsModule, RouterTestingModule],
+            declarations: [HeroFeaturedComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeroFeaturedComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HeroFeaturedComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
