b4e3b73b467d1bc8ae1e6cfc69ed3273872840cb
Liam Clarke <liam.clarke.94@outlook.com>
2019-08-04 13:20:13 +0100

    Add Card component unit test

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -3,23 +3,36 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CardComponent } from './card.component';
 
 describe('CardComponent', () => {
-  let component: CardComponent;
-  let fixture: ComponentFixture<CardComponent>;
+    let component: CardComponent;
+    let fixture: ComponentFixture<CardComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [CardComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CardComponent);
+        component = fixture.componentInstance;
+        component.news = ArticleStub;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
+
+const ArticleStub = {
+    source: { id: 'string', name: 12 },
+    author: 'string',
+    title: 'string',
+    description: 'string',
+    url: 'urlString',
+    urlToImage: 'image string',
+    publishedAt: '2019-07-19T00:19:50Z',
+    content: 'string',
+
+}
