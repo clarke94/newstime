d60045bbceec78a4ee359e7d2f08b35490d6dd4e
Liam Clarke <liam.clarke.94@outlook.com>
2019-08-04 14:04:19 +0100

    Add Search Page unit tests

--- src/app/pages/search/search.component.spec.ts
+++ src/app/pages/search/search.component.spec.ts
@@ -1,25 +1,49 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
+import { Component, Input } from '@angular/core';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Article } from 'src/app/models/article/article';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('SearchComponent', () => {
-  let component: SearchComponent;
-  let fixture: ComponentFixture<SearchComponent>;
+    let component: SearchComponent;
+    let fixture: ComponentFixture<SearchComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                NgxPaginationModule,
+                RouterTestingModule,
+                HttpClientModule
+            ],
+            declarations: [
+                SearchComponent,
+                HeroFeaturedStubComponent,
+                InfoboxStubComponent
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SearchComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
+
+@Component({ selector: 'app-hero-featured', template: '' })
+class HeroFeaturedStubComponent {
+    @Input() search;
+}
+
+@Component({ selector: 'app-infobox', template: '' })
+class InfoboxStubComponent {
+    @Input() result: Article;
+}
