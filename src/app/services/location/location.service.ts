84be43306c90e9000847728eaa17ff157e10eeb4
Liam Clarke <liam.clarke.94@outlook.com>
2019-08-06 19:40:04 +0100

    Fix local server IP request

--- src/app/services/location/location.service.ts
+++ src/app/services/location/location.service.ts
@@ -19,9 +19,9 @@ export class LocationService {
         const endpoint = 'ipgeo';
         const supportedCountries = ["ae", "ar", "at", "au", "be", "bg", "br", "ca", "ch", "cn", "co", "cu", "cz", "de", "eg", "fr", "gb", "gr", "hk", "hu", "id", "ie", "il", "in", "it", "jp", "kr", "lt", "lv", "ma", "mx", "my", "ng", "nl", "no", "nz", "ph", "pl", "pt", "ro", "rs", "ru", "sa", "se", "sg", "si", "sk", "th", "tr", "tw", "ua", "us", "ve", "za"];
         return this.http.get(`${this.ipUrl}/${endpoint}?${this.ipKey}`).pipe(
-            map(({ country_code2 }: any) => country_code2.toLowerCase()),
+            map(({ country_code2 }: any) => country_code2),
             map((code) => {
-                this.location = supportedCountries.includes(code) ? code : 'gb';
+                this.location = supportedCountries.map(x => x.toUpperCase()).includes(code) ? code : 'gb';
                 return this.location;
             })
         );
